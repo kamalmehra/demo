038b47676f1ecc38436aaa3daecd48cc4e80df78
Kamal Mehra <krrish.mehra6@gmail.com>
2020-08-20 10:08:05 +0530

    JENKINS

---
+++ src/main/java/HelloWorld/App.java
@@ -0,0 +1,23 @@
+package HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public App()
+    {
+        System.out.println("Constructor");
+    }
+    
+    public void m1()
+    {
+        String s="";
+    }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/HelloWorld/AppTest.java
@@ -0,0 +1,26 @@
+package HelloWorld;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    public void shouldAnswerWithFalse()
+    {
+    	assertFalse(false);
+    }
+}
